cbb675f9b401e37412ccaf599c2739401e15058a
achrizal15 <achrizal15@gmail.com>
2021-07-19 01:16:39 +0700

    filter data

--- lib/Page/home.dart
+++ lib/Page/home.dart
@@ -333,26 +333,6 @@ class _HomePageState extends State<HomePage> {
                   SizedBox(
                     height: 10,
                   ),
-                  Container(
-                    height: 1,
-                    width: double.maxFinite,
-                    color: Colors.grey.withOpacity(0.5),
-                  ),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  Container(
-                    alignment: Alignment.centerRight,
-                    child: InkWell(
-                      onTap: () {},
-                      child: Text(
-                        "Tampilkan Transaksi",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Color(0XFF00B686)),
-                      ),
-                    ),
-                  )
                 ],
               ),
             ),
